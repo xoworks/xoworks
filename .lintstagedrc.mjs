cb5a9be56103703675ef58ed07d6ae6c954a79ab
madebyjake <38321395+madebyjake@users.noreply.github.com>
2025-04-15 11:43:55 -0400

    refactor: improve lint-staged config

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,4 +1,6 @@
-export default {
+const lintStagedConfig = {
   '*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write'],
   '*.{json,md,yaml,yml}': ['prettier --write'],
 };
+
+export default lintStagedConfig;
